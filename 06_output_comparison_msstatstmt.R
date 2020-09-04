259eb7c0ec725866302b87f419be276d8591141b
Mateusz Staniak <mtst@mstaniak.pl>
2020-09-04 14:49:55 +0200

    updates comparison code

--- 06_output_comparison_msstatstmt.R
+++ 06_output_comparison_msstatstmt.R
@@ -1,68 +1,39 @@
 library(data.table)
 source("./02_utility_functions.R")
-getStatsSingleVersion = function(df, version) {
-    data.table(
-        version = version,
-        n_features = count_features(df),
-        n_proteins = count_proteins(df),
-        n_peptides = count_peptides(df),
-        n_infinite = count_infinite(df),
-        n_missing = count_missing_values(df),
-        n_zero = count_zero_values(df),
-        n_exactly_zero = count_exactly_zero(df),
-        n_rows = count_rows(df),
-        n_cols = count_cols(df),
-        n_conditions = count_design(df, "Condition"),
-        n_bioreps = count_design(df, "BioReplicate"),
-        n_runs = count_design(df, "Run"),
-        n_tech_replicates = count_design(df, "TechRep"),
-        n_tech_rep_mixture = count_design(df, "TechRepMixture"),
-        n_fractions = count_fractions(df),
-        n_channels = count_design(df, "Channel"))
-}
-getStats = function(v3, v4) {
-    rbindlist(
-        list(getStatsSingleVersion(as.data.table(v3), "v3"),
-             getStatsSingleVersion(as.data.table(as(v4, "data.frame")), "v4"))
-    )
-    # list(class(v3), class(v4))
-}
 # TMT-Plubell2016-OpenMS
 om_input_path = "./processed_data/TMT-Plubell2016-OpenMS/20200225_MSstatsTMT_OpenMS_Export.RDS"
 om_input = readRDS(om_input_path)
-om_v3 = readRDS("./single_outputs/TMT-Plubell2016-OpenMS_v3.RDS")
-head(om_v3)
+om_v3 = MSstatsTMT::OpenMStoMSstatsTMTFormat(om_input)
 om_v4 = MSstatsConvert::OpenMStoMSstatsTMTFormat(om_input)
-head(om_v4)
 # TMT-exampledata-SpectroMine
 sm_input_path = "./processed_data/TMT-exampledata-SpectroMine/exampledata_input.RDS"
 sm_annot_path = "./processed_data/TMT-exampledata-SpectroMine/SpectroMine_annotation.RDS"
 sm_input = readRDS(sm_input_path)
 sm_annot = readRDS(sm_annot_path)
-sm_v3 = readRDS("./single_outputs/TMT-exampledata-SpectroMine_v3.RDS")
+sm_v3 = MSstatsTMT::SpectroMinetoMSstatsTMTFormat(sm_input, sm_annot)
 sm_v4 = MSstatsConvert::SpectroMinetoMSstatsTMTFormat(sm_input, sm_annot)
 # TMT-Controlledmix-MS3-MaxQuant
-mq_ev_path = "./processed_data/TMT-Controlledmix-MS3-MaxQuant/evidence.RDS"
-mq_pg_path = "./processed_data/TMT-Controlledmix-MS3-MaxQuant/proteinGroups.RDS"
-mq_annot_path = "./processed_data/TMT-Controlledmix-MS3-MaxQuant/MaxQuant_annotation.RDS"
+mq_ev_path = "./processed_data/TMT-Controlledmix_MS3-MaxQuant/evidence.RDS"
+mq_pg_path = "./processed_data/TMT-Controlledmix_MS3-MaxQuant/proteinGroups.RDS"
+mq_annot_path = "./processed_data/TMT-Controlledmix_MS3-MaxQuant/MaxQuant_annotation.RDS"
 mq_ev = readRDS(mq_ev_path)
 mq_pg = readRDS(mq_pg_path)
 mq_annot = readRDS(mq_annot_path)
-mq_v3 = readRDS("./single_outputs/TMT-Controlledmix-MS3-MaxQuant_v3.RDS")
+mq_v3 = MSstatsTMT::MaxQtoMSstatsTMTFormat(mq_ev, mq_pg, mq_annot)
 mq_v4 = MSstatsConvert::MaxQtoMSstatsTMTFormat(mq_ev, mq_pg, mq_annot)
 # TMT-Controlledmix-SP3-PD
-pd_input_path = "./processed_data/TMT-Controlledmix-SP3-PD/spike_input.RDS"
-pd_annot_path = "./processed_data/TMT-Controlledmix-SP3-PD/SpikeIn5mix_PD_annotation.RDS"
+pd_input_path = "./processed_data/TMT-Controlledmix_SP3-PD/spike_input.RDS"
+pd_annot_path = "./processed_data/TMT-Controlledmix_SP3-PD/SpikeIn5mix_PD_annotation.RDS"
 pd_input = readRDS(pd_input_path)
 pd_annot = readRDS(pd_annot_path)
-pd_v3 = readRDS("./single_outputs/TMT-Controlledmix-SP3-PD_v3.RDS")
+pd_v3 = MSstatsTMT::PDtoMSstatsTMTFormat(pd_input, pd_annot)
 pd_v4 = MSstatsConvert::PDtoMSstatsTMTFormat(pd_input, pd_annot)
 
-ipsc_v3 = readRDS("./single_outputs/TMT_iPSC-PD_v3.RDS")
 ipsc_input_path = "./processed_data/TMT-iPSC-PD/ipsc_input.RDS"
 ipsc_annot_path = "./processed_data/TMT-iPSC-PD/ipsc_annotation.RDS"
 ipsc_input = readRDS(ipsc_input_path)
 ipsc_annot = readRDS(ipsc_annot_path)
+ipsc_v3 = MSstatsTMT::PDtoMSstatsTMTFormat(ipsc_input, ipsc_annot)
 ipsc_v4 = MSstatsConvert::PDtoMSstatsTMTFormat(ipsc_input, ipsc_annot)
 
 all_comps_tmt = rbindlist(list(
@@ -73,13 +44,3 @@ all_comps_tmt = rbindlist(list(
     cbind(tool = "PD_ipsc", getStats(ipsc_v3, ipsc_v4))
 ), fill = TRUE)
 all_comps_tmt
-
-ipsc_v4_check = as.data.table(ipsc_v4)
-ipsc_v4_check[, Mixture := as.factor(as.character(Mixture))]
-ipsc_v4_check[, TechRepMixture := as.factor(as.character(TechRepMixture))]
-ipsc_v4_check[, BioReplicate := as.factor(as.character(BioReplicate))]
-ipsc_compare = merge(ipsc_v4_check, as.data.table(ipsc_v3),
-                     by = setdiff(colnames(ipsc_v4), "Intensity"),
-                     all.x = TRUE)
-unique(ipsc_compare[is.na(Intensity.y) & !is.na(Intensity.x), .(PeptideSequence, Charge)])
-
--- 09_output_comparison_full.R
+++ 09_output_comparison_full.R
@@ -3,10 +3,29 @@ library(parallel)
 library(data.table)
 source("./02_utility_functions.R")
 metadata = readRDS("metadata.RDS")
-metadata = metadata[sapply(metadata, function(x) x$type) != "TMT"]
+# metadata = metadata[sapply(metadata, function(x) x$type) != "TMT"]
+
+# Additional options to check:
+# Remove 1 feature proteins
+# Do not remove few measurements
+get_comparison_df = function(path) {
+    all_files = list.files(path, full.names = TRUE)
+    files = data.table(file = all_files)
+    files[, dataset := gsub("v[0-9].RDS", "", file)]
+    files[, v := stringr::str_extract(file, "v[0-9]")]
+    splitted = split(files, files$dataset)
+    stats_lf = rbindlist(lapply(splitted, function(test) {
+        cbind(dataset = unique(gsub(paste0(path, "/"), "", test$dataset)),
+              getStats(readRDS(test[v == "v3", file]),
+                       readRDS(test[v == "v4", file])))
+    }))
+    stats_lf
+}
+
 
 problems = list()
-save_single_output = function(metadata) {
+save_single_output = function(metadata, output_path = "./single_outputs/", remove_single_feature = FALSE, remove_few = TRUE) {
+    remove_few_lf = ifelse(remove_few, "remove", "keep")
     for (dataset in metadata) {
         tryCatch({
             dataset = lapply(dataset, function(x) gsub("./datasets/", "", x))
@@ -15,27 +34,47 @@ save_single_output = function(metadata) {
                 protein_groups = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$protein_groups)))
                 annotation = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$annotation)))
                 if (dataset$type == "TMT") {
-                    v3 = MSstatsTMT::MaxQtoMSstatsTMTFormat(evidence, protein_groups, annotation)
-                    v4 = MSstatsConvert::MaxQtoMSstatsTMTFormat(evidence, protein_groups, annotation)
+                    v3 = MSstatsTMT::MaxQtoMSstatsTMTFormat(evidence, protein_groups, annotation,
+                                                            rmPSM_withfewMea_withinRun = remove_few,
+                                                            rmProtein_with1Feature = remove_single_feature)
+                    v4 = MSstatsConvert::MaxQtoMSstatsTMTFormat(evidence, protein_groups, annotation,
+                                                                rmPSM_withfewMea_withinRun = remove_few,
+                                                                rmProtein_with1Feature = remove_single_feature)
                 } else {
-                    v3 = MSstats::MaxQtoMSstatsFormat(evidence, annotation, protein_groups)
-                    v4 = MSstatsConvert::MaxQtoMSstatsFormat(evidence, annotation, protein_groups)
+                    v3 = MSstats::MaxQtoMSstatsFormat(evidence, annotation, protein_groups,
+                                                      removeProtein_with1Peptide = remove_single_feature,
+                                                      fewMeasurements = remove_few_lf)
+                    v4 = MSstatsConvert::MaxQtoMSstatsFormat(evidence, annotation, protein_groups,
+                                                             removeProtein_with1Peptide = remove_single_feature,
+                                                             fewMeasurements = remove_few_lf)
                 }
             } else if (dataset$tool == "DIAUmpire") {
                 fragments = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$fragment_path)))
                 peptides = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$peptide_path)))
                 proteins = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$protein_path)))
                 annotation = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$annotation)))
-                v3 = MSstats::DIAUmpiretoMSstatsFormat(fragments, peptides, proteins, annotation)
-                v4 = MSstatsConvert::DIAUmpiretoMSstatsFormat(fragments, peptides, proteins, annotation)
+                v3 = MSstats::DIAUmpiretoMSstatsFormat(fragments, peptides, proteins, annotation,
+                                                       removeProtein_with1Feature = remove_single_feature,
+                                                       fewMeasurements = remove_few_lf)
+                v4 = MSstatsConvert::DIAUmpiretoMSstatsFormat(fragments, peptides, proteins, annotation,
+                                                              removeProtein_with1Feature = remove_single_feature,
+                                                              fewMeasurements = remove_few_lf)
             } else if (dataset$tool == "OpenMS") {
                 input = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$input_path)))
                 if (dataset$type == "TMT") {
-                    v3 = MSstatsTMT::OpenMStoMSstatsTMTFormat(input)
-                    v4 = MSstatsConvert::OpenMStoMSstatsTMTFormat(input)
+                    v3 = MSstatsTMT::OpenMStoMSstatsTMTFormat(input,
+                                                              rmPSM_withfewMea_withinRun = remove_few,
+                                                              rmProtein_with1Feature = remove_single_feature)
+                    v4 = MSstatsConvert::OpenMStoMSstatsTMTFormat(input,
+                                                                  rmPSM_withfewMea_withinRun = remove_few,
+                                                                  rmProtein_with1Feature = remove_single_feature)
                 } else {
-                    v3 = MSstats::OpenMStoMSstatsFormat(input)
-                    v4 = MSstatsConvert::OpenMStoMSstatsFormat(input)
+                    v3 = MSstats::OpenMStoMSstatsFormat(input,
+                                                        removeProtein_with1Peptide = remove_single_feature,
+                                                        fewMeasurements = remove_few_lf)
+                    v4 = MSstatsConvert::OpenMStoMSstatsFormat(input,
+                                                               removeProtein_with1Peptide = remove_single_feature,
+                                                               fewMeasurements = remove_few_lf)
                 }
             } else {
                 input = readRDS(paste0("./processed_data/", gsub("tsv|csv|xls|txt", "RDS", dataset$input_path)))
@@ -45,41 +84,69 @@ save_single_output = function(metadata) {
 
                 if (dataset$tool == "PD") {
                     if (dataset$type == "TMT") {
-                        v3 = MSstatsTMT::PDtoMSstatsTMTFormat(input, annotation)
-                        v4 = MSstatsConvert::PDtoMSstatsTMTFormat(input, annotation)
+                        v3 = MSstatsTMT::PDtoMSstatsTMTFormat(input, annotation,
+                                                              rmPSM_withfewMea_withinRun = remove_few,
+                                                              rmProtein_with1Feature = remove_single_feature)
+                        v4 = MSstatsConvert::PDtoMSstatsTMTFormat(input, annotation,
+                                                                  rmPSM_withfewMea_withinRun = remove_few,
+                                                                  rmProtein_with1Feature = remove_single_feature)
                     } else {
-                        v3 = MSstats::PDtoMSstatsFormat(input, annotation)
-                        v4 = MSstatsConvert::PDtoMSstatsFormat(input, annotation)
+                        v3 = MSstats::PDtoMSstatsFormat(input, annotation,
+                                                        removeProtein_with1Peptide = remove_single_feature,
+                                                        fewMeasurements = remove_few_lf)
+                        v4 = MSstatsConvert::PDtoMSstatsFormat(input, annotation,
+                                                               removeProtein_with1Peptide = remove_single_feature,
+                                                               fewMeasurements = remove_few_lf)
                     }
                 }
 
                 if (dataset$tool == "OpenSWATH") {
-                    v3 = MSstats::OpenSWATHtoMSstatsFormat(input, annotation)
-                    v4 = MSstatsConvert::OpenSWATHtoMSstatsFormat(input, annotation)
+                    v3 = MSstats::OpenSWATHtoMSstatsFormat(input, annotation,
+                                                           removeProtein_with1Feature = remove_single_feature,
+                                                           fewMeasurements = remove_few_lf)
+                    v4 = MSstatsConvert::OpenSWATHtoMSstatsFormat(input, annotation,
+                                                                  removeProtein_with1Feature = remove_single_feature,
+                                                                  fewMeasurements = remove_few_lf)
                 }
 
                 if (dataset$tool == "Progenesis") {
-                    v3 = MSstats::ProgenesistoMSstatsFormat(input, annotation)
-                    v4 = MSstatsConvert::ProgenesistoMSstatsFormat(input, annotation)
+                    v3 = MSstats::ProgenesistoMSstatsFormat(input, annotation,
+                                                            removeProtein_with1Peptide = remove_single_feature,
+                                                            fewMeasurements = remove_few_lf)
+                    v4 = MSstatsConvert::ProgenesistoMSstatsFormat(input, annotation,
+                                                                   removeProtein_with1Peptide = remove_single_feature,
+                                                                   fewMeasurements = remove_few_lf)
                 }
 
                 if (dataset$tool == "Skyline") {
-                    v3 = MSstats::SkylinetoMSstatsFormat(input, annotation)
-                    v4 = MSstatsConvert::SkylinetoMSstatsFormat(input, annotation)
+                    v3 = MSstats::SkylinetoMSstatsFormat(input, annotation,
+                                                         removeProtein_with1Feature = remove_single_feature,
+                                                         fewMeasurements = remove_few_lf)
+                    v4 = MSstatsConvert::SkylinetoMSstatsFormat(input, annotation,
+                                                                removeProtein_with1Feature = remove_single_feature,
+                                                                fewMeasurements = remove_few_lf)
                 }
 
                 if (dataset$tool == "SpectroMine") {
-                    v3 = MSstatsTMT::SpectroMinetoMSstatsTMTFormat(input, annotation)
-                    v4 = MSstatsConvert::SpectroMinetoMSstatsTMTFormat(input, annotation)
+                    v3 = MSstatsTMT::SpectroMinetoMSstatsTMTFormat(input, annotation,
+                                                                   rmPSM_withfewMea_withinRun = remove_few,
+                                                                   rmProtein_with1Feature = remove_single_feature)
+                    v4 = MSstatsConvert::SpectroMinetoMSstatsTMTFormat(input, annotation,
+                                                                       rmPSM_withfewMea_withinRun = remove_few,
+                                                                       rmProtein_with1Feature = remove_single_feature)
                 }
 
                 if (dataset$tool == "Spectronaut") {
-                    v3 = MSstats::SpectronauttoMSstatsFormat(input, annotation)
-                    v4 = MSstatsConvert::SpectronauttoMSstatsFormat(input, annotation)
+                    v3 = MSstats::SpectronauttoMSstatsFormat(input, annotation,
+                                                             removeProtein_with1Feature = remove_single_feature,
+                                                             fewMeasurements = remove_few_lf)
+                    v4 = MSstatsConvert::SpectronauttoMSstatsFormat(input, annotation,
+                                                                    removeProtein_with1Feature = remove_single_feature,
+                                                                    fewMeasurements = remove_few_lf)
                 }
             }
-            saveRDS(v3, file = paste0("./single_outputs/", dataset$folder_path, "_", "v3", ".RDS"))
-            saveRDS(v4, file = paste0("./single_outputs/", dataset$folder_path, "_", "v4", ".RDS"))
+            saveRDS(v3, file = paste0(output_path, dataset$folder_path, "_", "v3", ".RDS"))
+            saveRDS(v4, file = paste0(output_path, dataset$folder_path, "_", "v4", ".RDS"))
         }, error = function(e) {
             problems <<- c(problems, dataset$folder_path)
             print(e)
@@ -87,19 +154,11 @@ save_single_output = function(metadata) {
     }
 }
 
-save_single_output(metadata)
+# metadata = metadata[sapply(metadata, function(x) x$tool) == "Skyline"]
+save_single_output(metadata, "./single_outputs/")
+save_single_output(metadata, output_path = "./single_outputs_no_few/", remove_single_feature = FALSE, remove_few = FALSE)
+save_single_output(metadata, output_path = "./single_outputs_no_singles/", remove_single_feature = TRUE, remove_few = TRUE)
 
-
-
-all_files = list.files("./single_outputs/", full.names = TRUE)
-files = data.table(file = all_files)
-files[, dataset := gsub("v[0-9].RDS", "", file)]
-files[, v := stringr::str_extract(file, "v[0-9]")]
-splitted = split(files, files$dataset)
-stats_lf = rbindlist(lapply(splitted, function(test) {
-    cbind(dataset = unique(gsub("./single_outputs//", "", test$dataset)),
-          getStats(readRDS(test[v == "v3", file]),
-             readRDS(test[v == "v4", file])))
-}))
-stats_lf
-
+compare_default = get_comparison_df("./single_outputs/")
+compare_no_few = get_comparison_df("./single_outputs_no_few/")
+compare_no_singles = get_comparison_df("./single_outputs_no_singles/")
